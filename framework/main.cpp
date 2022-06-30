1b7bb5cd7bf139030ce39d71cd6cc9081122e330
in-sang-cho <perrot_1@naver.com>
2022-06-30 15:50:01 +0900

    framework v7.0 Template

--- framework/main.cpp
+++ framework/main.cpp
@@ -3,9 +3,9 @@
 #include "Headers.h"
 #include "MainUpdate.h"
 
-
 int main(void)
 {
+
 	MainUpdate Main;
 	Main.Initialize();
 
@@ -34,4 +34,45 @@ int main(void)
 	scene 작성하니까 scenemanager 만들어야하고
 	scene 만들었으니 여러 scene들 만들고
 	scene 넘겨야하니까 input 만들어야하고
+*/
+
+// Template (템플릿)
+/*
+	자료형을 보내주는 매개변수
+	데이터 타입을 넘겨주는 역할을 한다.
+	사용할 때는 [이름의 끝]에 쓰고 괄호가 뒤에 붙는다.
+	함수에 썼으면 함수의 이름 끝에 붙이고, 클래스에 썼으면 클래스 이름 끝에 붙인다.
+*/
+
+/*
+template <typename T1, typename T2>
+class Temp
+{
+public:
+	static T* Func() return new T1;
+	static T* Func(int i) return new T1(i);
+	static T* Func(float f) return new T1(f);
+};
+
+class A
+{
+public:
+	int Number;
+};
+
+class B
+{
+public:
+	float Number;
+
+};
+
+int main(void)
+{
+	A* a = Temp<A, B>::Func();
+
+	a->Number = 10;
+
+	return 0;
+}
 */
